26a5e9179f738f9349fd01e28a733aaa2fe1da1a
suncl <258268936@qq.com>
2022-10-04 22:45:31 +0800

    允许跨域

---
+++ school-web/src/main/java/work/hdjava/shcoolweb/config/WebMvcConfigure.java
@@ -0,0 +1,12 @@
+package work.hdjava.shcoolweb.config;
+
+import org.springframework.context.annotation.Configuration; import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry; import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+ public class WebMvcConfigure implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
--- school-web/src/main/java/work/hdjava/shcoolweb/controller/RouterController.java
+++ school-web/src/main/java/work/hdjava/shcoolweb/controller/RouterController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/aaa")
 public class RouterController {
-	@RequestMapping(value = "/bbb")
-	public ModelAndView home() {
+	@RequestMapping(value = "/schools")
+	public ModelAndView schools() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Home");
 		return mv;
