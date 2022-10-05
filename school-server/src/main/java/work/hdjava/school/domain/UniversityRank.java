fa8d4036814b5720f8b8310d2e3d52ab96fd401d
suncl <258268936@qq.com>
2022-10-05 21:54:09 +0800

    增加地区等冗余字段

--- school-server/src/main/java/work/hdjava/school/domain/UniversityRank.java
+++ school-server/src/main/java/work/hdjava/school/domain/UniversityRank.java
@@ -1,17 +1,14 @@
 package work.hdjava.school.domain;
 
-import com.alibaba.excel.annotation.ExcelIgnore;
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.alibaba.excel.annotation.ExcelIgnore;import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
-import io.swagger.annotations.ApiModelProperty;
-
+import com.baomidou.mybatisplus.annotation.TableName;import io.swagger.annotations.ApiModelProperty;
 import java.util.Date;
 
 /**
  * Author suncl
- * Date: 2022/10/4 11:19
+ * Date: 2022/10/5 21:49
  * Description:  该文件的描述
  */
 @TableName(value = "university_rank")
@@ -21,6 +18,12 @@ public class UniversityRank {
      */
     @TableId(value = "id", type = IdType.INPUT)
     private Integer id;
+
+    /**
+     * 地区
+     */
+    @TableField(value = "area")
+    private String area;
 
     /**
      * 类型
@@ -65,6 +68,82 @@ public class UniversityRank {
     private Date opDate;
 
     /**
+     * 冗余1
+     */
+    @TableField(value = "extr1")
+    private String extr1;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr2")
+    private String extr2;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr3")
+    private String extr3;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr4")
+    private String extr4;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr5")
+    private String extr5;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr6")
+    private String extr6;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr7")
+    private String extr7;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr8")
+    private String extr8;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr9")
+    private String extr9;
+
+    /**
+     * 冗余1
+     */
+    @TableField(value = "extr10")
+    private String extr10;
+    @TableField(exist = false)
+    @ApiModelProperty(value = "当前页码")
+    @ExcelIgnore
+    private Long pageIndex = 1l;
+    @TableField(exist = false)
+    @ApiModelProperty(value = "页大小")
+    @ExcelIgnore
+    private Long pageSize = 10l;
+    @TableField(exist = false)
+    @ApiModelProperty(value = "起始时间")
+    @ExcelIgnore
+    private String startDate;
+    @TableField(exist = false)
+    @ApiModelProperty(value = "结束时间")
+    @ExcelIgnore
+    private String endDate;
+
+    /**
      * 获取自增主键
      *
      * @return id - 自增主键
@@ -80,6 +159,24 @@ public class UniversityRank {
      */
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    /**
+     * 获取地区
+     *
+     * @return area - 地区
+     */
+    public String getArea() {
+        return area;
+    }
+
+    /**
+     * 设置地区
+     *
+     * @param area 地区
+     */
+    public void setArea(String area) {
+        this.area = area;
     }
 
     /**
@@ -208,23 +305,185 @@ public class UniversityRank {
         this.opDate = opDate;
     }
 
+    /**
+     * 获取冗余1
+     *
+     * @return extr1 - 冗余1
+     */
+    public String getExtr1() {
+        return extr1;
+    }
 
-    @TableField(exist = false)
-    @ApiModelProperty(value = "当前页码")
-    @ExcelIgnore
-    private Long pageIndex = 1l;
-    @TableField(exist = false)
-    @ApiModelProperty(value = "页大小")
-    @ExcelIgnore
-    private Long pageSize = 10l;
-    @TableField(exist = false)
-    @ApiModelProperty(value = "起始时间")
-    @ExcelIgnore
-    private String startDate;
-    @TableField(exist = false)
-    @ApiModelProperty(value = "结束时间")
-    @ExcelIgnore
-    private String endDate;
+    /**
+     * 设置冗余1
+     *
+     * @param extr1 冗余1
+     */
+    public void setExtr1(String extr1) {
+        this.extr1 = extr1;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr2 - 冗余1
+     */
+    public String getExtr2() {
+        return extr2;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr2 冗余1
+     */
+    public void setExtr2(String extr2) {
+        this.extr2 = extr2;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr3 - 冗余1
+     */
+    public String getExtr3() {
+        return extr3;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr3 冗余1
+     */
+    public void setExtr3(String extr3) {
+        this.extr3 = extr3;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr4 - 冗余1
+     */
+    public String getExtr4() {
+        return extr4;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr4 冗余1
+     */
+    public void setExtr4(String extr4) {
+        this.extr4 = extr4;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr5 - 冗余1
+     */
+    public String getExtr5() {
+        return extr5;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr5 冗余1
+     */
+    public void setExtr5(String extr5) {
+        this.extr5 = extr5;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr6 - 冗余1
+     */
+    public String getExtr6() {
+        return extr6;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr6 冗余1
+     */
+    public void setExtr6(String extr6) {
+        this.extr6 = extr6;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr7 - 冗余1
+     */
+    public String getExtr7() {
+        return extr7;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr7 冗余1
+     */
+    public void setExtr7(String extr7) {
+        this.extr7 = extr7;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr8 - 冗余1
+     */
+    public String getExtr8() {
+        return extr8;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr8 冗余1
+     */
+    public void setExtr8(String extr8) {
+        this.extr8 = extr8;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr9 - 冗余1
+     */
+    public String getExtr9() {
+        return extr9;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr9 冗余1
+     */
+    public void setExtr9(String extr9) {
+        this.extr9 = extr9;
+    }
+
+    /**
+     * 获取冗余1
+     *
+     * @return extr10 - 冗余1
+     */
+    public String getExtr10() {
+        return extr10;
+    }
+
+    /**
+     * 设置冗余1
+     *
+     * @param extr10 冗余1
+     */
+    public void setExtr10(String extr10) {
+        this.extr10 = extr10;
+    }
 
 
     public Long getPageIndex() {
