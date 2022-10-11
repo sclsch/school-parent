f12115d54c7cd147cd1c3563dbf4d81df89d7569
suncl <258268936@qq.com>
2022-10-11 15:10:51 +0800

    包结构调整

--- school-gateway/src/main/java/work/hdjava/school/gateway/SchoolGatewayApplication.java
+++ school-gateway/src/main/java/work/hdjava/school/gateway/SchoolGatewayApplication.java
@@ -1,9 +1,16 @@
 package work.hdjava.school.gateway;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DruidDataSourceAutoConfigure.class,
+        DataSourceTransactionManagerAutoConfiguration.class
+})
 public class SchoolGatewayApplication {
 
     public static void main(String[] args) {
